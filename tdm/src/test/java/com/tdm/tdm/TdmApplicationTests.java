9571c1e2e1529c95acad6760480dedd443324108
kanagavel <kanagu2win@gmail.com>
2021-06-30 15:45:11 +0530

    First Commit

---
+++ tdm/src/test/java/com/tdm/tdm/TdmApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tdm.tdm;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdmApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
